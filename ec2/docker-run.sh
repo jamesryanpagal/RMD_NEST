a9bfd23c4480748d8721a7785d7f62a1b0a649b6
James Ryan Pagal <jamesryanpagal@Jamess-MacBook-Pro.local>
2025-10-28 03:12:02 +0800

    docker run exec

---
+++ ec2/docker-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONTAINER_NAME=rmdland-server-container
+IMAGE_NAME=rmdland-server
+
+# Stop & remove old container if it exists
+docker rm -f $CONTAINER_NAME 2>/dev/null
+
+# Build fresh image
+docker build -t $IMAGE_NAME .
+
+# Run container with .env
+docker run -d \
+  --name $CONTAINER_NAME \
+  --env-file .env \
+  -p 4000:4000 \
+  $IMAGE_NAME
