3bb8058277425cb710af1ac53aa7cd7a6f227691
James Ryan Pagal <jamesryanpagal@Jamess-MacBook-Pro.local>
2025-10-29 00:57:09 +0800

    updated function.sql and triggers.sql

--- functions.sql
+++ functions.sql
@@ -1,16 +1,3 @@
-CREATE OR REPLACE FUNCTION set_receipt_number_trigger() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-DECLARE
-	receipt_no VARCHAR(220);
-BEGIN
-
-	receipt_no := generate_receipt_number();
-	NEW."receiptNo" := receipt_no;
-	RETURN NEW;
-END;
-$$;
-
 CREATE OR REPLACE FUNCTION generate_receipt_number() RETURNS text
     LANGUAGE plpgsql
     AS $$BEGIN
--- triggers.sql
+++ triggers.sql
@@ -199,6 +199,23 @@ BEFORE UPDATE ON "Payment"
 FOR EACH ROW
 EXECUTE FUNCTION set_deleted_date();
 
+CREATE OR REPLACE TRIGGER payment_trigger 
+BEFORE INSERT ON "Payment" 
+FOR EACH ROW EXECUTE FUNCTION set_receipt_number_trigger();
+
+CREATE OR REPLACE FUNCTION set_receipt_number_trigger() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+	receipt_no VARCHAR(220);
+BEGIN
+
+	receipt_no := generate_receipt_number();
+	NEW."receiptNo" := receipt_no;
+	RETURN NEW;
+END;
+$$;
+
 CREATE OR REPLACE TRIGGER set_deleted_date_trigger
 BEFORE UPDATE ON "PaymentAudit"
 FOR EACH ROW
